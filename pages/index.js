61dca379cedc41e95c60210f5317bedb295d84ba
jessicagallagher <jessicalyngallagher@me.com>
2021-11-23 15:54:22 -0500

    linting

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,13 @@ export default function Home() {
       <Head>
         <title>Marathon Training</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin>
-            <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;300;400;500;600;700;800&family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-            </Head>
-            <main>
-              <LandingPage />
-            </main>
-          </div>
-          )
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;300;400;500;600;700;800&family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+      </Head>
+      <main>
+        <LandingPage />
+      </main>
+    </div>
+  )
 }
--- pages/users/[id].js
+++ pages/users/[id].js
@@ -33,22 +33,22 @@ export async function getStaticPaths() {
   };
 }
 
-export default ({ user }) => {
-  return (
-    <div>
-      <Head>
-        <title>Jessica</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;300;400;500;600;700;800&family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-      </Head>
+export default function({ user }) {
 
-      <main>
-        <Schedule />
-      </main>
+  <div>
+    <Head>
+      <title>Jessica</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;300;400;500;600;700;800&family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+    </Head>
 
-    </div>
+    <main>
+      <Schedule />
+    </main>
 
-  );
-}
+  </div>
+
+
+};
